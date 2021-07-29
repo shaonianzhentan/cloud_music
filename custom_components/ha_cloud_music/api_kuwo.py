c27b21e6e385c4344ff24538ed466906dd7a5fc7
sangood <zhangsangood@gmail.com>
2021-07-29 08:13:12 +0800

    Update api_kuwo.py

--- custom_components/ha_cloud_music/api_kuwo.py
+++ custom_components/ha_cloud_music/api_kuwo.py
@@ -9,21 +9,6 @@ from .kwDES import base64_encrypt
 import time
 
 
-# 获取HTTP请求信息
-# async def fetch_info(url):
-#     p = urlparse(url)
-#     headers = {
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-#         'Referer': f'{p.scheme}//{p.netloc}'
-#     }
-#     connector = aiohttp.TCPConnector(verify_ssl=False)
-#     async with aiohttp.ClientSession(headers=headers,connector=connector) as session:
-#         async with session.get(url) as response:
-#             return {
-#               'status': response.status,
-#               'url': str(response.url)
-#             }
-            
 ###################### 获取酷我音乐 ######################
 # 获取网易云 检测是否可用 
 async def check_163_song_url( id, songName, singerName, url):
