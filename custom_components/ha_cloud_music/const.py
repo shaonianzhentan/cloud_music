8ba621f6f75900689d158cdd0d510a11d1800ded
shaonianzhentan <ilovevar@gmail.com>
2022-06-18 00:47:42 +0800

    全新改版

--- custom_components/ha_cloud_music/const.py
+++
@@ -1,8 +0,0 @@
-NAME = '云音乐'
-ICON = 'mdi:music'
-DOMAIN = 'ha_cloud_music'
-VERSION = '4.7.7'
-DOMAIN_API = '/' + DOMAIN + '-api'
-WEB_PATH = '/' + DOMAIN + '-web'
-ROOT_PATH = '/' + DOMAIN + '-local/' + VERSION
-PLATFORMS = ["media_player"]
